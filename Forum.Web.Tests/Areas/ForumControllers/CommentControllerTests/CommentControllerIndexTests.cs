9e271873bb7f9621ea9f6a6fdac06c46a926ab27
Todor13 <todorstaykovski@gmail.com>
2017-03-18 19:49:09 +0200

    CommentControllerIndex tests

---
+++ Forum.Web.Tests/Areas/ForumControllers/CommentControllerTests/CommentControllerIndexTests.cs
@@ -0,0 +1,27 @@
+﻿using Forum.Data;
+using Forum.Web.Areas.Forum.Controllers;
+using Moq;
+using NUnit.Framework;
+using System.Web.Mvc;
+
+namespace Forum.Web.Tests.Areas.ForumControllers.CommentControllerTests
+{
+    [TestFixture]
+    public class CommentControllerIndexTests
+    {
+        [Test]
+        public void CommentController_Index_ShouldRenderCorrectPartialView()
+        {
+            // Arrange
+            var data = new Mock<IUowData>();
+
+            CommentController controller = new CommentController(data.Object);
+
+            // Act
+            PartialViewResult result = controller.Index() as PartialViewResult;
+
+            // Assert
+            Assert.AreEqual("_Comment", result.ViewName);
+        }
+    }
+}
